45d331353e2f693a8ffae775c07e29bf18549faa
Jesse Kempf <jesse.kempf@opower.com>
2014-07-28 21:23:27 -0700

    refactor protocol handler.

--- lib/sensu/socket.rb
+++ lib/sensu/socket.rb
@@ -1,5 +1,8 @@
 module Sensu
   class Socket < EM::Connection
+
+    class DataError < StandardError; end
+
     attr_accessor :logger, :settings, :transport, :reply
 
     def respond(data)
@@ -9,9 +12,17 @@ module Sensu
     end
 
     def receive_data(data)
-      if data.bytes.find { |char| char > 0x80 }
-        @logger.warn('socket received non-ascii characters')
+      begin
+        process_data(data)
+      rescue DataError => exception
+        @logger.warn(exception.to_s)
         respond('invalid')
+      end
+    end
+
+    def process_data(data)
+      if data.bytes.find { |char| char > 0x80 }
+        fail(DataError, 'socket received non-ascii characters')
       elsif data.strip == 'ping'
         @logger.debug('socket received ping')
         respond('pong')
@@ -23,27 +34,18 @@ module Sensu
           check = MultiJson.load(data)
           check[:issued] = Time.now.to_i
           check[:status] ||= 0
-          validates = [
-            check[:name] =~ /^[\w\.-]+$/,
-            check[:output].is_a?(String),
-            check[:status].is_a?(Integer)
-          ].all?
-          if validates
-            payload = {
-              :client => @settings[:client][:name],
-              :check => check
-            }
-            @logger.info('publishing check result', {
-              :payload => payload
-            })
-            @transport.publish(:direct, 'results', MultiJson.dump(payload))
-            respond('ok')
-          else
-            @logger.warn('invalid check result', {
-              :check => check
-            })
-            respond('invalid')
-          end
+
+          self.class.validate_check_data(check)
+
+          payload = {
+            :client => @settings[:client][:name],
+            :check => check
+          }
+          @logger.info('publishing check result', {
+            :payload => payload
+          })
+          @transport.publish(:direct, 'results', MultiJson.dump(payload))
+          respond('ok')
         rescue MultiJson::ParseError => error
           @logger.warn('check result must be valid json', {
             :data => data,
@@ -51,6 +53,27 @@ module Sensu
           })
           respond('invalid')
         end
+      end
+    end
+
+    def self.validate_check_data(check)
+      #
+      # Basic sanity checks.
+      #
+      fail(DataError, "invalid check name: '#{check[:name]}'") unless check[:name] =~ /^[\w\.-]+$/
+      fail(DataError, "check output must be a String, got #{check[:output].class.name} instead") unless check[:output].is_a?(String)
+
+      #
+      # Status code validation.
+      #
+      status_code = check[:status]
+
+      unless status_code.is_a?(Integer)
+        fail(DataError, "check status must be an Integer, got #{status_code.class.name} instead") unless status_code.is_a?(Integer)
+      end
+
+      unless 0 <= status_code && status_code <= 3
+        fail(DataError, "check status must be in {0, 1, 2, 3}, got #{status_code} instead")
       end
     end
   end
--- spec/socket_spec.rb
+++ spec/socket_spec.rb
@@ -16,7 +16,7 @@ describe Sensu::Socket do
     {
       :name => 'o-hai',
       :output => 'DEADBEEF' * 2,
-      :status => 0,
+      :status => 3,
     }
   end
 
@@ -60,11 +60,66 @@ describe Sensu::Socket do
       subject.receive_data('a relentless stream of garbage')
     end
 
-    it 'is validated as a check' do
-      check_report_data.delete(:output)
+    it 'must contain a non-empty check name' do
+      check_report_data.merge!(:name => '')
 
       expect(logger).to receive(:debug).with('socket received data', { :data => check_report_data.to_json })
-      expect(logger).to receive(:warn).with('invalid check result', { :check => check_report_data.merge(:issued => 1234) })
+      expect(logger).to receive(:warn).with("invalid check name: ''")
+
+      expect(subject).to receive(:respond).with('invalid')
+
+      subject.receive_data(check_report_data.to_json)
+    end
+
+    it 'must contain an acceptable check name' do
+      check_report_data.merge!(:name => 'o hai')
+
+      expect(logger).to receive(:debug).with('socket received data', { :data => check_report_data.to_json })
+      expect(logger).to receive(:warn).with("invalid check name: 'o hai'")
+
+      expect(subject).to receive(:respond).with('invalid')
+
+      subject.receive_data(check_report_data.to_json)
+    end
+
+    it 'must have check output that is a string' do
+      check_report_data.merge!(:output => 1234)
+
+      expect(logger).to receive(:debug).with('socket received data', { :data => check_report_data.to_json })
+      expect(logger).to receive(:warn).with('check output must be a String, got Fixnum instead')
+
+      expect(subject).to receive(:respond).with('invalid')
+
+      subject.receive_data(check_report_data.to_json)
+    end
+
+    it 'must have an integer status' do
+      check_report_data.merge!(:status => '1234')
+
+      expect(logger).to receive(:debug).with('socket received data', { :data => check_report_data.to_json })
+      expect(logger).to receive(:warn).with('check status must be an Integer, got String instead')
+
+      expect(subject).to receive(:respond).with('invalid')
+
+      subject.receive_data(check_report_data.to_json)
+    end
+
+    it 'must have a status code in the valid range' do
+      check_report_data.merge!(:status => -2)
+
+      expect(logger).to receive(:debug).with('socket received data', { :data => check_report_data.to_json })
+      expect(logger).to receive(:warn).with('check status must be in {0, 1, 2, 3}, got -2 instead')
+
+      expect(subject).to receive(:respond).with('invalid')
+
+      subject.receive_data(check_report_data.to_json)
+    end
+
+    it 'must have a status code in the valid range' do
+      check_report_data.merge!(:status => 4)
+
+      expect(logger).to receive(:debug).with('socket received data', { :data => check_report_data.to_json })
+      expect(logger).to receive(:warn).with('check status must be in {0, 1, 2, 3}, got 4 instead')
 
       expect(subject).to receive(:respond).with('invalid')
 
